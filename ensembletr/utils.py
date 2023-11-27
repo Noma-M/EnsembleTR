093231f87c1b65550eefe40aacc17e3c20fc87c8
Noma-M <77622537+Noma-M@users.noreply.github.com>
2023-11-27 14:26:53 +0200

    Update utils.py

--- ensembletr/utils.py
+++ ensembletr/utils.py
@@ -30,7 +30,7 @@ def GetEHScore(conf_invs, CNs, ru_len):
         print(f"Invalid copy numbers or intervals: {CNs}, {conf_invs}")
         return 0
     score1 = CalcEHAlleleScore(conf_invs[0], CNs[0])
-        return 0.8 * min(score1) + 0.2 * max(score1)
+    return 0.8 * min(score1) + 0.2 * max(score1)
 
 def CalcEHAlleleScore(conf_inv, allele):
     r"""
